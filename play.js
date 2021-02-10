7f455f6d321fb6677b5006ca89ddf9d1e62bb4b5
Neil Oribello <neil.oribello@gmail.com>
2021-02-10 20:56:59 +0000

    added module.exports to connect with play.js

--- play.js
+++ play.js
@@ -1,22 +1,4 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  conn.on('data', (data) => {
-    console.log(data);
-  })
-
-  return conn;
-}
-
+// play.js
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
