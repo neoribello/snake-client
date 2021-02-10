5d604e77666946005435ea6d91d7ecb50a47ec46
Neil Oribello <neil.oribello@gmail.com>
2021-02-10 20:22:03 +0000

    putting client into a seperate module

---
+++ client.js
@@ -0,0 +1,12 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
